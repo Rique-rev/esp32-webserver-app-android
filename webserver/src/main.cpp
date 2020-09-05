29b36bb6212de767cbcdce1b851165691fa89bff
Henrique Ribeiro Evangelista <henrique.rev@gmail.com>
2020-09-05 17:29:03 -0300

    Add esqueleto endpoints GET POST

--- webserver/src/main.cpp
+++ webserver/src/main.cpp
@@ -1,6 +1,5 @@
-#define WIFI_SSID "*********"
-#define WIFI_PSK  "*********"
-
+#define WIFI_SSID "**********"
+#define WIFI_PSK "**********"
 
 // We will use wifi
 #include <WiFi.h>
@@ -13,38 +12,43 @@ using namespace httpsserver; // PORTA DEFAULT: 443
 
 HTTPServer server = HTTPServer();
 
+void handleRoot(HTTPRequest *req, HTTPResponse *res);
+void handle404(HTTPRequest *req, HTTPResponse *res);
 
-void handleRoot(HTTPRequest * req, HTTPResponse * res);
-void handle404(HTTPRequest * req, HTTPResponse * res);
-
-void setup() {
+void setup()
+{
 
   Serial.begin(115200);
 
-
   Serial.println("Setting up WiFi");
   WiFi.begin(WIFI_SSID, WIFI_PSK);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(500);
   }
   Serial.print("Connected. IP=");
   Serial.println(WiFi.localIP());
 
-  ResourceNode * nodeRoot    = new ResourceNode("/", "GET", &handleRoot);
-  ResourceNode * node404     = new ResourceNode("", "GET", &handle404);
+  ResourceNode *nodeRootGET = new ResourceNode("/", "GET", &handleRoot);
+  server.registerNode(nodeRootGET);
 
-  server.registerNode(nodeRoot);
+  ResourceNode *nodeRootPOST = new ResourceNode("/", "POST", &handleRoot);
+  server.registerNode(nodeRootPOST);
+
+  ResourceNode *node404 = new ResourceNode("", "GET", &handle404);
   server.setDefaultNode(node404);
 
   Serial.println("Starting server...");
   server.start();
-  if (server.isRunning()) {
+  if (server.isRunning())
+  {
     Serial.println("Server ready.");
   }
 }
 
-void loop() {
+void loop()
+{
   // This call will let the server do its work
   server.loop();
 
@@ -52,31 +56,55 @@ void loop() {
   delay(1);
 }
 
-void handleRoot(HTTPRequest * req, HTTPResponse * res) {
-  
-  res->setHeader("Content-Type", "text/html");
+void handleRoot(HTTPRequest *req, HTTPResponse *res)
+{
 
-  res->println("<!DOCTYPE html>");
-  res->println("<html>");
-  res->println("<head><title>Hello World!</title></head>");
-  res->println("<body>");
-  res->println("<h1>Hello World!</h1>");
-  res->print("<p>Your server is running for ");
-  // A bit of dynamic data: Show the uptime
-  res->print((int)(millis()/1000), DEC);
-  res->println(" seconds.</p>");
-  res->println("</body>");
-  res->println("</html>");
+  if (req->getMethod() == "GET")
+  {
+    res->setHeader("Content-Type", "text/html");
+
+    res->println("<!DOCTYPE html>");
+    res->println("<html>");
+    res->println("<head><title>Hello World!</title></head>");
+    res->println("<body>");
+    res->println("<h1>Hello World!</h1>");
+    res->print("<p>Your server is running for ");
+    // A bit of dynamic data: Show the uptime
+    res->print((int)(millis() / 1000), DEC);
+    res->println(" seconds.</p>");
+    res->println("</body>");
+    res->println("</html>");
+  }
+  else if (req->getMethod() == "POST")
+  {
+    DynamicJsonDocument bodyJson(256);
+
+    res->setHeader("Content-Type", "text/plain");
+
+    // Pegando o tamanho (em bytes) do body
+    const int bufferSize = req->getContentLength();
+    // Criando um buffer (variavel que armazenara o conteudo em bytes do body) do tamanho correto
+    byte buffer[bufferSize];
+
+    while (!(req->requestComplete()))
+    {
+      // Lendo os bytes do body e armazenando dentro do buffer
+      req->readBytes(buffer, bufferSize);
+    }
+
+    // Convertendo o conteudo do buffer em string (conjunto de chars)
+    String bodyStr;
+    for (char c : buffer)
+      bodyStr += c;
+
+    Serial.println(bodyStr);
+
+    res->print(bodyStr);
+  }
 }
 
-// void handleFavicon(HTTPRequest * req, HTTPResponse * res) {
-//   // Set Content-Type
-//   res->setHeader("Content-Type", "image/vnd.microsoft.icon");
-//   // Write data from header file
-//   res->write(FAVICON_DATA, FAVICON_LENGTH);
-// }
-
-void handle404(HTTPRequest * req, HTTPResponse * res) {
+void handle404(HTTPRequest *req, HTTPResponse *res)
+{
   // Discard request body, if we received any
   // We do this, as this is the default node and may also server POST/PUT requests
   req->discardRequestBody();
@@ -94,5 +122,4 @@ void handle404(HTTPRequest * req, HTTPResponse * res) {
   res->println("<head><title>Not Found</title></head>");
   res->println("<body><h1>404 Not Found</h1><p>The requested resource was not found on this server.</p></body>");
   res->println("</html>");
-
 }
